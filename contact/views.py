7bedfed14cd8ece410dfc5d139e93593336340a4
David Downes <me@theukdave.com>
2016-10-24 22:46:06 +0100

    Use the request session to store success data of the form submission, rather than using get params
    Updated comments

--- contact/views.py
+++ contact/views.py
@@ -1,21 +1,33 @@
 from django.shortcuts import redirect
 from django.views.generic.edit import FormView
 from django.views.generic import View, TemplateView
-from django.core.urlresolvers import reverse
+from django.core.urlresolvers import reverse_lazy
 from django.utils.http import urlquote
 
-from .forms import DITHelpForm, FeedbackForm
+from .forms import FeedbackForm
 
 
-class DITHelpView(FormView):
+class DITHelpViewMixin():
+    """
+    A mixin that handles populating a subclass of DITHelpForm, setting the initial data for the form, instructing the
+    form to submit a ticket to zendesk, and storing success data in the session for use in the default success page.
+
+    The minimum to make this mixin work, is to include it in a view that also inherits from FormView, and then set the
+    form_class to a form that itself inherits from DITHelpForm.
+    """
+
     template_name = 'help.html'
-    form_class = DITHelpForm
+    success_url = reverse_lazy('contact:thanks')
 
     def form_valid(self, form):
-        origin_page = form.cleaned_data.get('originating_page')
+        originating_page = form.cleaned_data.get('originating_page')
         success, code = form.raise_zendesk_ticket()
-        redirect_url = "{0}?success={1}&code={2}&redirect={3}"
-        return redirect(redirect_url.format(reverse('contact:thanks'), success, code, urlquote(origin_page)))
+        self.request.session['success_data'] = {
+            'originating_page': originating_page,
+            'success': success,
+            'code': code
+        }
+        return super().form_valid(form)
 
     def get_form_kwargs(self):
         kwargs = super().get_form_kwargs()
@@ -24,18 +36,13 @@ class DITHelpView(FormView):
         return kwargs
 
 
-class FeedbackView(DITHelpView):
-    form_class = FeedbackForm
-
-
 class ThanksView(TemplateView):
     template_name = 'thanks.html'
 
     def get_context_data(self, *args, **kwargs):
-        context = {
-            'success': self.request.GET.get('success') == 'True',
-            'code': int(self.request.GET.get('code')),
-            'origin_page': self.request.GET.get('redirect'),
-        }
-
+        context = {'success_data': self.request.session['success_data']}
         return context
+
+
+class FeedbackView(DITHelpViewMixin, FormView):
+    form_class = FeedbackForm
