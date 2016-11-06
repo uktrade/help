3bc996ea5af7eb828a184c355fde75ece2c21cda
David Downes <me@theukdave.com>
2016-11-06 13:29:39 +0000

    Adding in the new triage form, which includes a select field (requiring new template) Factored out labels, choices, and help texts into a meta module Improved templates for inputs so that attributes can be specified Provide a basic get_body method on the form that newline separates all of the forms field labels with their values
    PEP8

--- contact/templatetags/utils.py
+++ contact/templatetags/utils.py
@@ -1,12 +1,14 @@
 from django import template
-from django.forms.widgets import TextInput, EmailInput, Textarea
+from django.forms.widgets import TextInput, EmailInput, Textarea, Select, NumberInput
 
 register = template.Library()
 
 
 @register.simple_tag
 def get_form_field_template(boundfield):
-    if type(boundfield.field.widget) in [TextInput, EmailInput]:
+    if type(boundfield.field.widget) in [TextInput, EmailInput, NumberInput]:
         return 'includes/inputfield.html'
+    elif type(boundfield.field.widget) == Select:
+        return 'includes/selectfield.html'
     elif type(boundfield.field.widget) == Textarea:
         return 'includes/textarea.html'
--- contact/urls.py
+++ contact/urls.py
@@ -6,5 +6,6 @@ app_name = 'contact'
 urlpatterns = [
     url(r'^feedback/thanks$', views.FeedbackThanksView.as_view(), name='feedback_thanks'),
     url(r'^feedback/(?P<service>[-\w\d]+)/$', views.FeedbackView.as_view(), name='feedback_submit'),
+    url(r'^triage/(?P<service>[-\w\d]+)/$', views.TriageView.as_view(), name='triage_submit'),
     url(r'^thanks/$', views.ThanksView.as_view(), name='thanks'),
 ]
