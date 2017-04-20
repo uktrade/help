65a42bee70ad3360eaed2a97790cb7e228fddf52
David Downes <me@theukdave.com>
2017-04-20 12:25:18 +0100

    Use the whitelister’s internal properties to figure out whether or not to add admin urls

--- help/urls.py
+++ help/urls.py
@@ -2,15 +2,14 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf import settings
 
+from ip_restriction import IpWhitelister
+
 
 urlpatterns = []
 
-RESTRICT_IPS = getattr(settings, 'RESTRICT_IPS', None)
+ip = IpWhitelister()
 
-if RESTRICT_IPS is None:
-    RESTRICT_IPS = os.environ.get('RESTRICT_IPS', '').lower() == 'true' or os.environ.get('RESTRICT_IPS') == '1'
-
-if RESTRICT_IPS:
+if ip.RESTRICT_IPS and ip.ALLOW_ADMIN:
     # If we're restricting IPs, we want the admin site active to log in to,
     # but otherwise it is not needed, and shouldn't be accessible
     urlpatterns += [
