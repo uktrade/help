d84e5f4a49d986df8277914f37c818071bf5270b
David Downes <me@theukdave.com>
2017-06-30 12:59:18 +0100

    change the input fields hint to match GDS forms add the originating page to the context to provide a 'back' link

--- contact/meta/help_text.py
+++ contact/meta/help_text.py
@@ -1,19 +1,15 @@
-COMPANY_NAME = "Enter the legal name of your business (not the trading name) then use 'Search Companies\
-                House' and select your company from the list"
-COMPANY_WEBSITE = "Website address, where we can see your products and services online."
+COMPANY_NAME = "Enter the legal name of your business (not the trading name) then use 'Search Companies House'\
+                and select your company from the list. It will then prefill your company number and postcode."
+COMPANY_WEBSITE = "Website address, where we can see your products online."
+COMPANY_NUMBER = "The 8 digit number you received when registering your company at Companies House."
 
-TRIAGE_TURNOVER = "You may use 12 months rolling or last year's annual turnover"
-TRIAGE_SKU_NUMBER = "A stock keeping unit (SKU) is a product identification code used for stock keeping.\
-                     Some marketplaces need businesses to have a minimum number of SKUs before they can join."
-TRIAGE_TRADEMARKED = "Some marketplaces will only sell products that are trademarked"
+TRIAGE_TURNOVER = "You may use 12 months rolling or last year's annual turnover."
+TRIAGE_SKU_NUMBER = "A stock keeping unit is an individual item, such as a product or a service\
+                    that is offered for sale."
+TRIAGE_TRADEMARKED = "Some marketplaces will only sell products that are trademarked."
 TRIAGE_DESCRIPTION = "Your pitch is important and the information you provide may be used to introduce you to the\
                       marketplace. You could describe your business including your products, your customers and how\
                       you market your products in a few paragraphs."
 TRIAGE_DESCRIPTION = "Your pitch is important and the information you provide may be used to introduce you to the\
-                      marketplace. You may wish to include the following points in your pitch:\
-                      <ul>\
-                          <li>Description of your business and your products</li>\
-                          <li>Your customers</li>\
-                          <li>How you market your products</li>\
-                          <li>Whether you have previous experience of selling on online marketplaces</li>\
-                      </ul>"
+                    marketplace.<br /><br />You could describe your business, including your products, your\
+                    customers and how you market your products in a few paragraphs."
