911f93d09ae9858fe74e6b6eb3273d77bd421369
Dermot Byrne <dermotbyrne@dermot.local>
2017-10-06 18:28:03 +0100

    (feature) getting footer up there

--- help/settings/base.py
+++ help/settings/base.py
@@ -124,7 +124,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 ZENDESK_URL = os.environ.get('ZENDESK_URL')
 ZENDESK_USER = os.environ.get('ZENDESK_USER')
